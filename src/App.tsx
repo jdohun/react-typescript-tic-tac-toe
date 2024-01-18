521cc38d2b50430abe86af163b1ee3b9618c79b6
jdohun <wjdehgns97@naver.com>
2024-01-18 14:01:38 +0900

    feat: Delete unnecessary duplicated code line

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,19 @@ import React, {useState} from 'react';
 import './styles.css'
 
 const Game = () => {
-    const [xIsNext, setXIsNext] = useState<boolean>(true);
     const [history, setHistory] = useState<Array<Array<string | null>>>([Array(9).fill(null)]);
     const [currentMove, setCurrentMove] = useState<number>(0);
+    const xIsNext = currentMove % 2 === 0;
     const currentSquares = history[currentMove];
 
     const handlePlay = (nextSquares: Array<string | null>) => {
         const nextHistory: Array<Array<string | null>> = [...history.slice(0, currentMove + 1), nextSquares];
         setHistory(nextHistory);
         setCurrentMove(nextHistory.length - 1);
-        setXIsNext(!xIsNext);
     }
 
     const jumpTo = (nextMove: number) => {
         setCurrentMove(nextMove);
-        setXIsNext(nextMove % 2 === 0);
     }
 
     const moves: JSX.Element[] = history.map((squares, move) => {
